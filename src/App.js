b349d6201ce71e4f2f3d505eb9f111007215a796
Kevin Seghetti <kts@tenetti.org>
2020-08-14 12:17:37 -0700

    added pixijs example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import BunnyExample from "./BunnyExample";
+import Stats from "./Stats";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Stats />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">react-pixi-fiber Examples</h1>
+        </header>
+        <div className="App-intro">
+          <BunnyExample />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Bunny/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Sprite } from "react-pixi-fiber";
+import * as PIXI from "pixi.js";
+import bunnys from "./bunnys.png";
+
+const centerAnchor = new PIXI.Point(0.5, 0.5);
+
+const bunnyTextures = new PIXI.Texture.from(bunnys);
+const textures = [
+  new PIXI.Texture(bunnyTextures.baseTexture, new PIXI.Rectangle(2, 47, 26, 37)),
+  new PIXI.Texture(bunnyTextures.baseTexture, new PIXI.Rectangle(2, 86, 26, 37)),
+  new PIXI.Texture(bunnyTextures.baseTexture, new PIXI.Rectangle(2, 125, 26, 37)),
+  new PIXI.Texture(bunnyTextures.baseTexture, new PIXI.Rectangle(2, 164, 26, 37)),
+  new PIXI.Texture(bunnyTextures.baseTexture, new PIXI.Rectangle(2, 2, 26, 37)),
+];
+
+function Bunny(props) {
+  const texture = textures[props.texture];
+  const Component = props.as;
+  return <Component anchor={centerAnchor} {...props} texture={texture} />;
+}
+Bunny.propTypes = {
+  as: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  texture: PropTypes.number,
+};
+Bunny.defaultProps = {
+  as: Sprite,
+  texture: 0,
+};
+
+export default Bunny;
