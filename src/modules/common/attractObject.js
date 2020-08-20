869f0e93042aadf317d7aab929c13415f3b9515d
Kevin Seghetti <kts@tenetti.org>
2020-08-20 09:16:20 -0700

    added attract object

---
+++ src/modules/common/attractObject.js
@@ -0,0 +1,33 @@
+// modules/common/attractObject.js
+
+import { CreateGameObject } from 'modules/common/gameObject'
+
+//===============================================================================
+
+export const AttractTick = (object,delta,clipping,keys,Callbacks,collisionList,state) =>
+{
+    console.log("AttractTick",keys)
+    if(keys.s)
+    {
+        Callbacks.StartNewGame()
+    }
+    return object
+}
+
+//===============================================================================
+
+export const CreateAttractObject = () =>
+{
+    let object = CreateGameObject(
+        'attract',
+        0,0,0,
+        0,5,0,
+        null
+    )
+    object.baseTick = object.tick
+    object.tick = AttractTick
+    return object
+}
+
+//===============================================================================
+
