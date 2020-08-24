0da2334a34b0a04af0b1c3efdbe6f59559537b62
Kevin Seghetti <kts@tenetti.org>
2020-08-24 16:08:52 -0700

    minor formatting changes

--- src/components/Shaders/index.js
+++ src/components/Shaders/index.js
@@ -9,16 +9,18 @@ import image from 'components/images/bg_scene_rotate.jpg'
 
 const geometry = new PIXI.Geometry()
     .addAttribute('aVertexPosition', // the attribute name
-        [-100, -100, // x, y
+        [  -100, -100, // x, y
             100, -100, // x, y
-            100, 100,
-            -100, 100], // x, y
+            100,  100,
+           -100,  100
+        ], // x, y
         2) // the size of the attribute
     .addAttribute('aUvs', // the attribute name
-        [0, 0, // u, v
+        [   0, 0, // u, v
             1, 0, // u, v
             1, 1,
-            0, 1], // u, v
+            0, 1
+        ], // u, v
         2) // the size of the attribute
     .addIndex([0, 1, 2, 0, 2, 3])
 
